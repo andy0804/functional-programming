91c5c0f863f66a9e4cb1e5b9b300edf73e4b0f8c
Ananth Kumbla <ananth.kumbla@gmail.com>
2023-11-23 08:36:41 -0600

    Update usinglodashcond.js

--- usinglodashcond.js
+++ usinglodashcond.js
@@ -9,9 +9,16 @@ const otherwise = () => true; // _.stubTrue
 const outputNumber = number => number // _.identity 
 
 
-const fizzBuzz1 = _.cond([
-    [isMultipleOf5And3, outputFizzBuzz],
-    [isMultipleOf3, outputFizz],
-    [isMultipleOf5, outputBuzz],
-    [otherwise, outputNumber ]
+const shouldFireFizzBuzz = input => isMultipleOf3(input) && isMultipleOf5(input);
+const shouldFireFizz = input => isMultipleOf3(input) && !isMultipleOf5(input); 
+// Bonus: const isNotMultipleOf5 = _.negate(isMultipleOf5);
+const shouldFireBuzz = input => isMultipleOf5(input) && !isMultipleOf3(input);
+const shouldReturnInput = input => !isMultipleOf5(input) && !isMultipleOf3(input)
+
+
+const fizzBuzz2 = _.cond([
+    [shouldFireFizzBuzz, outputFizzBuzz],
+    [shouldFireFizz, outputFizz],
+    [shouldFireBuzz, outputBuzz],
+    [shouldReturnInput, outputNumber ]
 ]);
